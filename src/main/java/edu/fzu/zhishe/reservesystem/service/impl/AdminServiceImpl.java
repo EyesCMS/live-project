727ac7375b3719ccff895fb0c8fe1e9c5b240976
xjliang <gnulxj@gmail.com>
2020-03-15 17:09:07 +0800

    fix(login): login failed

--- src/main/java/edu/fzu/zhishe/reservesystem/service/impl/AdminServiceImpl.java
+++ src/main/java/edu/fzu/zhishe/reservesystem/service/impl/AdminServiceImpl.java
@@ -1,9 +1,12 @@
 package edu.fzu.zhishe.reservesystem.service.impl;
 
+import static javax.swing.UIManager.get;
+
 import edu.fzu.zhishe.reservesystem.generator.AdminUser;
 import edu.fzu.zhishe.reservesystem.generator.AdminUserDao;
 import edu.fzu.zhishe.reservesystem.generator.AdminUserExample;
 import edu.fzu.zhishe.reservesystem.service.AdminService;
+import java.util.List;
 import javax.annotation.Resource;
 import org.springframework.stereotype.Service;
 
@@ -22,6 +25,7 @@ public class AdminServiceImpl implements AdminService {
         adminUserExample.createCriteria()
             .andUserNameEqualTo(name)
             .andUserPswEqualTo(password);
-        return adminUserDao.selectByExample(adminUserExample).get(0);
+        List<AdminUser> adminUsers = adminUserDao.selectByExample(adminUserExample);
+        return adminUsers.isEmpty() ? null : adminUsers.get(0);
     }
 }
