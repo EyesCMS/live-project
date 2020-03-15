312b91a1ade4944804b32d69cbff68c8a019163f
xjliang <gnulxj@gmail.com>
2020-03-15 22:21:51 +0800

    feat(task): display list of tasks

--- src/main/java/edu/fzu/zhishe/reservesystem/service/TaskService.java
+++ src/main/java/edu/fzu/zhishe/reservesystem/service/TaskService.java
@@ -1,12 +1,15 @@
 package edu.fzu.zhishe.reservesystem.service;
 
 import edu.fzu.zhishe.reservesystem.generator.Task;
+import java.util.List;
 
 public interface TaskService {
 
     boolean started(Integer id);
 
     boolean finished(Integer id);
+
+    List<Task> findAll();
 
     Task findById(Integer id);
 
