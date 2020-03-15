2f14cc25ccd18a7d9803e04b261586e0a9760983
yz990219 <50985454+yz990219@users.noreply.github.com>
2020-03-15 19:35:51 +0800

    commit_yz_01
    adminController.java增加task对象的构造，增加admin表单的name

--- src/main/java/edu/fzu/zhishe/reservesystem/controller/AdminController.java
+++ src/main/java/edu/fzu/zhishe/reservesystem/controller/AdminController.java
@@ -1,7 +1,16 @@
 package edu.fzu.zhishe.reservesystem.controller;
 
+import edu.fzu.zhishe.reservesystem.generator.Task;
+import edu.fzu.zhishe.reservesystem.generator.TaskDao;
+import edu.fzu.zhishe.reservesystem.generator.TaskExample;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @Controller
 public class AdminController {
@@ -9,4 +18,29 @@ public class AdminController {
     public String admin() {
         return "admin";
     }
+
+    @PostMapping("/admin")
+    public String admin(@RequestParam(value = "starDate") String startDate,
+                        @RequestParam(value = "endDate") String endDate,
+                        @RequestParam(value = "starTime") String startTime,
+                        @RequestParam(value = "endTime") String endTime,
+                        @RequestParam(value = "max_num") int maxSingleNum,
+                        @RequestParam(value = "total_num") int maxTotalNum) throws ParseException {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm");
+        Date startFullDate = simpleDateFormat.parse(startDate + " " + startTime);
+        Date endFullDate = simpleDateFormat.parse(endDate + " " + endTime);
+        Task task = new Task();
+        task.setStartTime(startFullDate);
+        task.setEndTime(endFullDate);
+        task.setMaxNum(maxSingleNum);
+        task.setTotalNum(maxTotalNum);
+        System.out.println(startFullDate);
+        System.out.println(endFullDate);
+        System.out.println(maxSingleNum);
+        System.out.println(maxTotalNum);
+        //int count = TaskDao.insert();
+        return "admin";
+
+    }
 }
+
