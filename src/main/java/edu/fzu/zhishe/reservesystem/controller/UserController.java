e81989b02f26fb34bfcf5b83f9121b0c6a98387e
wjh674 <674593004@qq.com>
2020-03-15 19:16:56 +0800

    user页面修改
    user页面表单name属性的添加；UserController增加post方法

--- src/main/java/edu/fzu/zhishe/reservesystem/controller/UserController.java
+++ src/main/java/edu/fzu/zhishe/reservesystem/controller/UserController.java
@@ -1,12 +1,31 @@
 package edu.fzu.zhishe.reservesystem.controller;
 
+import edu.fzu.zhishe.reservesystem.generator.OrderList;
+import edu.fzu.zhishe.reservesystem.service.OrderService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class UserController {
+
+    private OrderService orderService;
+
     @RequestMapping("/user")
     public String user() {
         return "user";
     }
+
+   /*
+    @PostMapping("/user")
+    public String doOrder(@RequestParam(value="name") String name,
+                          @RequestParam(value="idNumber") String idNum,
+                          @RequestParam(value="tel") String tel,
+                          @RequestParam(value="maskNum") String num)
+    {
+
+    }
+
+    */
 }
