7f997729143dc9deb7c99d1db50e4f33ff5bf952
yz990219 <50985454+yz990219@users.noreply.github.com>
2020-03-16 21:09:26 +0800

    commit_yz_09
    将admin控制器中的判定部分改到服务层

--- src/main/java/edu/fzu/zhishe/reservesystem/service/TaskService.java
+++ src/main/java/edu/fzu/zhishe/reservesystem/service/TaskService.java
@@ -1,6 +1,8 @@
 package edu.fzu.zhishe.reservesystem.service;
 
 import edu.fzu.zhishe.reservesystem.generator.Task;
+
+import java.text.ParseException;
 import java.util.List;
 
 public interface TaskService {
@@ -14,4 +16,8 @@ public interface TaskService {
     Task findById(Integer id);
 
     int insertByTask(Task task);
+
+    boolean isLegalInput(String startDate,String endDate,String startTime,String endTime,String maxSingleNum,String maxTotalNum) throws ParseException;
+
+    Task taskCreate(String startDate,String endDate,String startTime,String endTime,String maxSingleNum,String maxTotalNum) throws ParseException;
 }
